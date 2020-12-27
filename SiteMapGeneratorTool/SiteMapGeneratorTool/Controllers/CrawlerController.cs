6d0151e1707406e9b1ecaa7890d815097b0c9a9f
tomoddy <tom_oddy@live.co.uk>
2020-12-27 13:11:01 +0000

    Create crawler api controller

---
+++ SiteMapGeneratorTool/SiteMapGeneratorTool/Controllers/CrawlerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteMapGeneratorTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrawlerController : ControllerBase
+    {
+        [HttpGet("start")]
+        public string Start(string url)
+        {
+            return "Crawling " + url;
+        }
+    }
+}
