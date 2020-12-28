8afdc54feee4cfe049409b965f586dc43326e596
tomoddy <tom_oddy@live.co.uk>
2020-12-28 13:16:51 +0000

    Added generate end point

---
+++ SiteMapGeneratorTool/SiteMapGeneratorTool/Controllers/GenerateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteMapGeneratorTool.Controllers
+{
+    public class GenerateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
