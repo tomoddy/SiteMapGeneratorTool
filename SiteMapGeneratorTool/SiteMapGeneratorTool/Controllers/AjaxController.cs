19441ac0a2050a8034d5d201a614115c6ca1a600
tomoddy <tom_oddy@live.co.uk>
2020-12-31 18:05:53 +0000

    Added javascript refresh for when pages are generated

---
+++ SiteMapGeneratorTool/SiteMapGeneratorTool/Controllers/AjaxController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SiteMapGeneratorTool.Helpers;
+using SiteMapGeneratorTool.Models;
+using SiteMapGeneratorTool.WebCrawler;
+using System.IO;
+
+namespace SiteMapGeneratorTool.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly FirebaseHelper FirebaseHelper;
+        private readonly S3Helper S3Helper;
+
+        public AjaxController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            FirebaseHelper = new FirebaseHelper(Configuration.GetValue<string>("Firebase:BasePath"), Configuration.GetValue<string>("Firebase:AuthSecret"));
+            S3Helper = new S3Helper(Configuration.GetValue<string>("AWS:Credentials:AccessKey"), Configuration.GetValue<string>("AWS:Credentials:SecretKey"), Configuration.GetValue<string>("AWS:S3:BucketName"));
+        }
+
+        public IActionResult Generate(string guid)
+        {
+            bool complete = S3Helper.FileExists(guid, Configuration.GetValue<string>("AWS:S3:Files:Information"));
+            ResultsModel model = new ResultsModel
+            {
+                Guid = guid,
+                Valid = FirebaseHelper.UserExists(guid),
+                Complete = complete,
+                Information = complete ? S3Helper.DownloadObject<Crawler>(guid, new FileInfo(Configuration.GetValue<string>("AWS:S3:Files:Information"))) : null
+            };
+
+            if (complete)
+                return new JsonResult(model);
+            else
+                return StatusCode(202);
+        }
+    }
+}
--- SiteMapGeneratorTool/SiteMapGeneratorTool/Controllers/GenerateController.cs
+++ SiteMapGeneratorTool/SiteMapGeneratorTool/Controllers/GenerateController.cs
@@ -1,25 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using SiteMapGeneratorTool.Helpers;
-using SiteMapGeneratorTool.Models;
-using SiteMapGeneratorTool.WebCrawler;
-using System.IO;
 
 namespace SiteMapGeneratorTool.Controllers
 {
     public class GenerateController : Controller
     {
-        private readonly IConfiguration Configuration;
-        private readonly FirebaseHelper FirebaseHelper;
-        private readonly S3Helper S3Helper;
-
-        public GenerateController(IConfiguration configuration)
-        {
-            Configuration = configuration;
-            FirebaseHelper = new FirebaseHelper(Configuration.GetValue<string>("Firebase:BasePath"), Configuration.GetValue<string>("Firebase:AuthSecret"));
-            S3Helper = new S3Helper(Configuration.GetValue<string>("AWS:Credentials:AccessKey"), Configuration.GetValue<string>("AWS:Credentials:SecretKey"), Configuration.GetValue<string>("AWS:S3:BucketName"));
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -27,15 +11,7 @@ namespace SiteMapGeneratorTool.Controllers
 
         public IActionResult Results(string guid)
         {
-            bool complete = S3Helper.FileExists(guid, Configuration.GetValue<string>("AWS:S3:Files:Information"));
-
-            ViewBag.Message = new ResultsModel
-            {
-                Guid = guid,
-                Valid = FirebaseHelper.UserExists(guid),
-                Complete = complete,
-                Information = complete ? S3Helper.DownloadObject<Crawler>(guid, new FileInfo(Configuration.GetValue<string>("AWS:S3:Files:Information"))) : null
-            };
+            ViewBag.Message = guid;
             return View();
         }
     }
