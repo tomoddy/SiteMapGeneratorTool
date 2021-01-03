74f16a253cd28276ebdeb13c51b5efba8513d3d7
tomoddy <tom_oddy@live.co.uk>
2021-01-03 06:50:29 +0000

    Added tests for request and response controller (#3)

---
+++ SiteMapGeneratorTool/SiteMapGeneratorToolTests/Controllers/WebCrawler/RequestControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+
+namespace SiteMapGeneratorTool.Controllers.WebCrawler.Tests
+{
+    [TestFixture()]
+    public class RequestControllerTests
+    {
+        IConfiguration Configuration;
+
+        [SetUp]
+        public void Init()
+        {
+            Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        }
+
+        [Test()]
+        public void IndexTest()
+        {
+            RequestController requestController = new RequestController(Configuration, new NullLogger<RequestController>());
+            requestController.Index("", "", false, false);
+        }
+    }
+}
