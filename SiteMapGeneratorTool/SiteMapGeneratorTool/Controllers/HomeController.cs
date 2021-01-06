5ea7870c5a3c5946f48e4fd911d49975f99c2f33
tomoddy <tom_oddy@live.co.uk>
2021-01-06 15:55:21 +0000

    Removed unused references

--- SiteMapGeneratorTool/SiteMapGeneratorTool/Controllers/HomeController.cs
+++ SiteMapGeneratorTool/SiteMapGeneratorTool/Controllers/HomeController.cs
@@ -2,12 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using NReco.ImageGenerator;
 using SiteMapGeneratorTool.Models;
-using System;
-using System.IO;
 using System.Diagnostics;
-using System.Net;
 
 namespace SiteMapGeneratorTool.Controllers
 {
