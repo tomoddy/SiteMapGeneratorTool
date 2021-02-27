b10c4600485616d12fa51bbe7a35fb2743f7179e
Tom Oddy <tom_oddy@live.co.uk>
2021-02-27 20:50:33 +0000

    Fixed decrypt scripts

--- SiteMapGeneratorTool/SiteMapGeneratorTool/decrypt_secret.sh
+++ SiteMapGeneratorTool/SiteMapGeneratorTool/decrypt_secret.sh
@@ -1,8 +1,2 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$APP_SETTINGS_PASSWORD" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output appsettings.json appsettings.json.gpg
