d598fce820af3a81cc53de85e61a3aa75f659a73
Tom Oddy <tom_oddy@live.co.uk>
2021-02-27 21:08:01 +0000

    Updated permissions

--- SiteMapGeneratorTool/SiteMapGeneratorTool/decrypt_secret.sh
+++ SiteMapGeneratorTool/SiteMapGeneratorTool/decrypt_secret.sh
@@ -1,1 +1,2 @@
-gpg --quiet --batch --yes --decrypt --passphrase="$APP_SETTINGS_PASSWORD" --output SiteMapGeneratorTool/SiteMapGeneratorTool/appsettings.json SiteMapGeneratorTool/SiteMapGeneratorTool/appsettings.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$APP_SETTINGS_PASSWORD" \
+--output SiteMapGeneratorTool/SiteMapGeneratorTool/appsettings.json SiteMapGeneratorTool/SiteMapGeneratorTool/appsettings.json.gpg
