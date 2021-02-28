c79c18079cdfcb8092ab66eb5a29859437ca2dda
Tom Oddy <tom_oddy@live.co.uk>
2021-02-28 12:20:57 +0000

    Added tests for privacy page (#21)

---
+++ SiteMapGeneratorTool/SiteMapGeneratorToolSelenium/Tests/Privacy.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace SiteMapGeneratorToolSelenium.Tests
+{
+    [TestFixture]
+    public class Privacy : Base
+    {
+        [SetUp]
+        public void HistorySetup()
+        {
+            ClickById("privacyLink");
+        }
+
+        [Test]
+        public void PrivacyPolicy()
+        {
+            TextEqualById("Use this page to detail your site's privacy policy.", "privacyInformation");
+            TextEqualById("Privacy Policy", "privacyMessage");
+        }
+    }
+}
